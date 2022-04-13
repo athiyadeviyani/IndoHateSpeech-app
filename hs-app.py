7acb05ca9fa9759dccac82d87b75177f70b53140
athiyadeviyani <adeviyani@cmu.edu>
2022-04-12 23:46:17 -0400

    fix requirements maybe

--- hs-app.py
+++ hs-app.py
@@ -6,6 +6,8 @@ import pickle
 
 PREDICT = False 
 
+st.beta_set_page_config(page_title='IndoHateSpeech')
+
 # LOAD MODELS
 cat_model = pickle.load(open('rf_cat.sav', 'rb'))
 hs_model = pickle.load(open('rf_hs.sav', 'rb'))
